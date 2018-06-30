11f47ed99f07761b0b43f3fa89ad5d170f537ead
Thomas Duncan <thomasbduncan@doctors.org.uk>
2018-06-30 13:41:47 +0100

    model updates

--- pt_list/models.py
+++ pt_list/models.py
@@ -4,13 +4,29 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
-class patient(models.Model):
+class Patient(models.Model):
 	hosp_number = models.CharField(max_length=20)
 	surname = models.CharField(max_length=50)
+	version_number = models.IntegerField(defualt=1)
 
-class patient_problems(models.Model):
-	patient = models.ForeignKey(patient, on_delete=models.PROTECT)
+class Patient_problems(models.Model):
+	patient = models.ForeignKey(Patient, on_delete=models.PROTECT)
+	problem = models.CharField(max_length=255)
+	version_number = models.IntegerField(defualt=1)
+
+class Medical_history(models.Model):
+	patient = models.ForeignKey(Patient, on_delete=models.PROTECT)
+	history_item = models.CharField(max_length=255)
+	version_number = models.IntegerField(defualt=1)
 
 
-class medical_history(models.Model):
-	patient = models.ForeignKey(patient, on_delete=models.PROTECT)
+class Job_Status(models.Model):
+	job_status_description = models.CharField(max_length=255)
+
+class Job(models.Model):
+	patient = models.ForeignKey(Patient, on_delete=models.PROTECT)
+	job = models.CharField(max_length=255)
+	job_status = models.ForeignKey(Job_Status, on_delete=models.PROTECT)
+	version_number = models.IntegerField(default=1)
+	
+
