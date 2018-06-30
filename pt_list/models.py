26efea382cdb3ce8259f9e3f020a45a0b4ce220e
Catalin Ursachi <catalin.ursachi@softwire.com>
2018-06-30 13:08:10 +0100

    Fixed Python style (#1)

--- pt_list/models.py
+++ pt_list/models.py
@@ -4,13 +4,15 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
-class patient(models.Model):
-	hosp_number = models.CharField(max_length=20)
-	surname = models.CharField(max_length=50)
 
-class patient_problems(models.Model):
-	patient = models.ForeignKey(patient, on_delete=models.PROTECT)
+class Patient(models.Model):
+    hosp_number = models.CharField(max_length=20)
+    surname = models.CharField(max_length=50)
 
 
-class medical_history(models.Model):
-	patient = models.ForeignKey(patient, on_delete=models.PROTECT)
+class PatientProblems(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.PROTECT)
+
+
+class MedicalHistory(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.PROTECT)
