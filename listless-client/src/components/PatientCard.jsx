bc5cd14d247098319c9f91ab696666f99f885983
Emily Feinson <Emily.Feinson@softwire.com>
2018-07-01 13:41:01 +0100

    Add patient priorities.

--- listless-client/src/components/PatientCard.jsx
+++ listless-client/src/components/PatientCard.jsx
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
 import getAge from 'get-age';
 
+import { patientPriorities, patientPriorityText } from "../utils/enums";
+
 import './PatientCard.css';
 
 export default class PatientCard extends Component {
 
+  getPriorityClass(priority) {
+    switch (priority) {
+      case patientPriorities.HIGH:
+        return 'high';
+      case patientPriorities.FIT_FOR_DISCHARGE:
+        return 'fit';
+      default:
+        return '';
+    }
+  }
+
   render() {
     const { patient } = this.props;
     const patientAge = getAge(patient.DateOfBirth);
+    const priorityClass = this.getPriorityClass(patient.PatientPriority);
 
     return (
       <div className="PatientCard-container">
+        <div className={`PatientCard-priority-${priorityClass}`}>
+          <p className="PatientCard-priority-text">{patientPriorityText(patient.PatientPriority)}</p>
+        </div>
         <div className="PatientCard-patient-details">
           <p>{patient.LocationWard} {patient.LocationBay}.{patient.LocationBed}</p>
           <p>{patient.FirstName} {patient.LastName}</p>
--- listless-client/src/utils/enums.js
+++ listless-client/src/utils/enums.js
@@ -1,5 +1,22 @@
 export const jobStatuses = Object.freeze({
-  notDone: 1,
-  inProgress: 2,
-  done: 3
+  NOT_DONE: 1,
+  IN_PROGRESS: 2,
+  DONE: 3
 });
+
+export const patientPriorities = Object.freeze({
+  HIGH: 1,
+  STANDARD: 2,
+  FIT_FOR_DISCHARGE: 3
+});
+
+export function patientPriorityText(priority) {
+  switch (priority) {
+    case patientPriorities.HIGH:
+      return 'High';
+    case patientPriorities.FIT_FOR_DISCHARGE:
+      return 'Medically fit for discharge';
+    default:
+      return null;
+  }
+}
