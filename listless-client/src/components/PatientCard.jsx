e262b263bb420eff8a1373b386bef22a6fd044d5
Emily Feinson <Emily.Feinson@softwire.com>
2018-07-01 10:28:14 +0100

    Add all information to view patients page. Temporarily fill in placeholders for properties not returned from the API.

--- listless-client/src/components/PatientCard.jsx
+++ listless-client/src/components/PatientCard.jsx
@@ -3,24 +3,37 @@ import React, { Component } from 'react';
 import './PatientCard.css';
 
 export default class PatientCard extends Component {
+
   render() {
     const { patient } = this.props;
+
+    // TODO: remove this once the API contains the correct info
+    patient.Age = 80;
+    patient.FirstName = 'John';
+    patient.Ward = '7';
+    patient.Bed = '03';
+    patient.Gender = 'F';
 
     return (
       <div className="PatientCard-container">
         <div className="PatientCard-patient-details">
-            <p>{patient.LastName}</p>
-            <p>{patient.HospitalNumber}</p>
+          <p>{patient.Ward} / {patient.Bed}</p>
+          <p>{patient.FirstName} {patient.LastName}</p>
+          <p><span className="PatientCard-gender">{patient.Gender}</span> {patient.Age}</p>
+          <p>{patient.HospitalNumber}</p>
         </div>
         <div className="PatientCard-medical-history">
+          <h2>Medical History</h2>
             <p>{patient.MedicalHistory}</p>
         </div>
         <div className="PatientCard-current-problems">
-            {patient.Problems
-                .filter(problem => problem.Active)
-                .map(problem => <p key={problem.id}>{problem.Problem}</p>)}
+          <h2>Problems</h2>
+          {patient.Problems
+              .filter(problem => problem.Active)
+              .map(problem => <p key={problem.id}>{problem.Problem}</p>)}
         </div>
         <div className="PatientCard-jobs">
+          <h2>Jobs</h2>
             {patient.Jobs.map(job => <p key={job.Id}>{job.Job}</p>)}
         </div>
       </div>
